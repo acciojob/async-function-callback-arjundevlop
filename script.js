0dbc2eff1635259ca18b9749312f77aca67bba60
Arjun Singh Tomar <109807311+arjundevlop@users.noreply.github.com>
2023-03-16 12:43:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
+const button = document.getElementById("btn");
+const outputDiv = document.getElementById("output");
+async function fetchData(url){
+	const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+	var data = await response.json();
+	outputDiv.innerHTML = data.title;
+}
+button.addEventListener("click",fetchData);
 
 //your JS code here. If required.
